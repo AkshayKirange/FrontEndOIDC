589a2446f22dcac9fad984df0124f52c88225daa
unknown <Administrator@Course-A31.cdacmumbai.in>
2022-07-08 10:34:44 +0530

    Code pushed
    Basic Login form along with redirection to e-pramaan is working, MongoDB is used in DB

---
+++ login-and-register-frontend/src/components/epramaan/epramaan.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./epramaan.css"
+
+const Epramaan = ({updateUser}) => {
+    return (
+        <div className="epramaan">
+            <h1>logged in using epramman</h1>
+           
+        </div>
+    )
+}
+
+export default Epramaan
